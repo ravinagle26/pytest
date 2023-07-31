09ac3fd1279671629420269d499d7654f53f9169
ravinagle26 <141040406+ravinagle26@users.noreply.github.com>
2023-08-01 00:30:16 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello World!'
+        return 'Hello World Have A Great Day!'
 
 api.add_resource(Greeting, '/') # Route_1
 
